d5605832cf2fe13485ab77a23a6bace236800bb8
fernandopassaia <fernandopassaia@futuradata.com.br>
2019-12-01 13:09:12 +0100

    Create Flutter File with Info

--- flutter/gasolina_ou_alcool1/lib/main.dart
+++ flutter/gasolina_ou_alcool1/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gasolina_ou_alcool1/widgets/logo.widget.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -28,12 +29,27 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
         body: ListView(
-          children: <Widget>[Logo()],
+          children: <Widget>[
+            Logo(),
+            TextFormField(
+              keyboardType: TextInputType.number,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 45,
+                fontFamily: "Big Shoulders Display",
+              ),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+              ),
+            ),
+          ],
         ));
   }
 }
