27989557a7883f878d62c8c209db90b069272ad9
fernandopassaia <fernandopassaia@futuradata.com.br>
2019-12-19 23:30:49 +0100

    Tela de Login toda desenhada, perfeita, iniciando comunicação com BackEnd

--- javascript/omnistack9_102019/mobile/src/pages/Login.js
+++ javascript/omnistack9_102019/mobile/src/pages/Login.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, KeyboardAvoidingView, Platform, Image, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import logo from '../assets/logo.png';
 
 // Existem duas maneiras de carregar ibagens:
 // <Image source={logo} /> 
 // <Image source={require("./images/logo.png")} />
 
+//Keyboard type vai mostrar o teclado com ARROBA pra digitar email AutoCapitalize = false não vai tentar corrigir por que é um campo de email
+//AutoCapitalize = words irá deixar cada letra inicial maíusculo. AutoCorrect desabilita correção automática (útil no campo email)
+
+//KeyboardAvoidingView joga todo o layout pra CIMA quando você clica num campo e o teclado sobe (no IOS isso não é default, no Android sim)
+
 export default function Login() {
     return (
-        <View style={StyleSheet.container}>
+        <KeyboardAvoidingView enabled={Platform.OS === 'ios'} behavior="padding" style={styles.container}>
             <Image source={logo} />
-            <Text style={styles.label}>SEU EMAIL *</Text>
-        </View>
+
+            <View style={styles.form}>
+                <Text style={styles.label}>SEU EMAIL *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Seu email"
+                    placeholderTextColor="#999"
+                    keyboardType="email-address"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                />
+
+                <Text style={styles.label}>TECNOLOGIAS *</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Tecnologias de interesse"
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Encontrar spots</Text>
+                </TouchableOpacity>
+            </View>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -20,5 +49,42 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+
+    form: {
+        alignSelf: 'stretch',
+        paddingHorizontal: 30,
+        marginTop: 30,
+    },
+
+    label: {
+        fontWeight: 'bold',
+        color: '#444',
+        marginBottom: 8,
+    },
+
+    input: {
+        borderWidth: 1,
+        borderColor: '#ddd',
+        paddingHorizontal: 20,
+        fontSize: 16,
+        color: '#444',
+        height: 44,
+        marginBottom: 20,
+        borderRadius: 2
+    },
+
+    button: {
+        height: 42,
+        backgroundColor: '#f05a5b',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 2,
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
     }
 });
