0e90a8b54aa58502f1c739c721767bd99e1e8b83
fernandopassaia <fernandopassaia@futuradata.com.br>
2019-12-20 21:40:59 +0100

    First Separated Component

---
+++ javascript/omnistack9_102019/mobile/src/components/SpotList.js
@@ -0,0 +1,9 @@
+// CRIEI UM COMPONENTE SEPARADO PARA AQUELA LISTA DE SPOTS QUE O USUÁRIO PODE SOLICITAR A RESERVA
+// COMO O COMPONENTE IRÁ SE REPETIR NA TELA, EU VOU TER ELE EM UM COMPONENTE SEPARADO.
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SpotList() {
+    return <Text>Spot List</Text>
+}
--- javascript/omnistack9_102019/mobile/src/pages/List.js
+++ javascript/omnistack9_102019/mobile/src/pages/List.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, AsyncStorage, StyleSheet, Image } from 'react-native';
+import logo from '../assets/logo.png';
+import { SafeAreaView } from 'react-navigation';
+import SpotList from '../components/SpotList';
 
 export default function List() {
-    return <View />
+    const [techs, setTechs] = useState([]);
+
+    // as tecnologias virão em formato de string separado por virgula, irei converter pra array
+    // o map irá pegar cada item desse array e irá tirar o espaço após a virgula
+    // useEffect(() = > {}, []) estrutura básica do useEffect, tem uma função (o que será feito) e um array de variáveis
+    // se estiver vazio, ele só executará uma vez ao abrir a tela. Se tiver algo, ele iŕa re-executar caso a variável se atualize.
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+            setTechs(techsArray);
+        })
+    }, []);
+
+    return (
+        <SafeAreaView>
+            <Image style={styles.logo} source={logo} />
+            <SpotList />
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: "contain",
+        alignSelf: 'center',
+        marginTop: 30
+    },
+});
