b96c1c3adaba712f944e20c07eb4f017a54dc4e6
fernandopassaia <fernandopassaia@futuradata.com.br>
2020-04-10 10:59:55 +0200

    Sample for Angular with MatTable Ordening

--- angular/mattable_ordening/git/src/app/directives/scollprogress.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ScollprogressDirective } from './scollprogress.directive';
-
-describe('ScollprogressDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ScollprogressDirective();
-    expect(directive).toBeTruthy();
-  });
-});
